8ecf3c6b849ae8c646a4a7074254137a6562177a
”Joonas <joonas.kinn@gmail.com>
2019-11-20 19:33:34 +0200

    add task 6.16

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import Filter from './components/Filter';
+import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => props.initializeAnecdotes(anecdotes))
+  }, [props])
+
   return (
     <div>
       <h1>Anecdotes</h1>
@@ -16,4 +24,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
